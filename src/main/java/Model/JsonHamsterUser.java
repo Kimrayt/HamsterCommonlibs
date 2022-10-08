9c8c90c651fbf68f43eef381e593254dd2089d65
Kimrayt <103435959+Kimrayt@users.noreply.github.com>
2022-10-08 16:18:43 +0300

    Made second producer for user and methods for user model

--- src/main/java/Model/JsonHamsterUser.java
+++ src/main/java/Model/JsonHamsterUser.java
@@ -1,12 +1,12 @@
 package Model;
 
 import lombok.*;
-import org.hibernate.annotations.Generated;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.management.relation.Role;
 import javax.persistence.Id;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -16,10 +16,32 @@ import java.util.Set;
 @Document
 public class JsonHamsterUser {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer id;
+    private volatile long id;
+    @Indexed
     private String username;
+    @Indexed
     private String email;
+    @Indexed
     private String password;
-    private Set userRoles;
+
+    @Indexed
+    private Set<String> roles = new HashSet<>();
+
+    public JsonHamsterUser(String username, String email, String password, Set<String> roles) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+
+    @Override
+    public String toString() {
+        return "JsonHamsterUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
